ee5b656bb8c3a5025e028d3483cb02c56a780408
Saumitra Aditya <saumitraaditya@users.noreply.github.com>
2017-01-27 16:00:09 -0500

    Create RabinKarp.py

---
+++ RabinKarp.py
@@ -0,0 +1,45 @@
+#! /usr/bin/python
+
+
+
+R = 256
+prime = 100007
+
+def hash(string_item,len_):
+	H = 0
+	for i in xrange(0,len_):
+		H = (R*H + ord(string_item[i]))%prime
+	return H
+	
+
+
+def RM(len_):
+	rm = 1
+	for i in range(1,len_):
+		rm = rm*R
+	return rm
+	
+	
+
+def find_pattern(input_string, pattern):
+	m = len(pattern)
+	pat_hash = hash(pattern,m)
+	curr_hash = hash(input_string,m)
+	rm = RM(m)
+	if (curr_hash == pat_hash):
+		print ("match found")
+		return
+	for i in range(m,len(input_string)):
+		curr_hash = (curr_hash + prime - (rm * ord(input_string[i-m]))%prime)%prime
+		curr_hash = (curr_hash*R+ord(input_string[i]))%prime
+		if (curr_hash == pat_hash):
+			print ("match found")
+			return
+	print ("match not found")
+			
+
+
+input_string = "my name is saumitra"
+pattern = "my"
+
+find_pattern(input_string,pattern)
